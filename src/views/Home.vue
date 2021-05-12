d8879baae902d462178ed2fe5af351d58033250e
Jumpei Asanuma <jun64pei89@gmail.com>
2021-05-12 21:29:48 +0900

    add: 勤怠アプリ？
    https://zenn.dev/hirotann/articles/1cac3a30af19a2837958

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <button @click="entry">出社</button>
+    <button @click="leave">退社</button>
+    <ul>
+      <li v-for="(data, index) in datas.data" :key="index">
+        {{ data }} | {{ index }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import { reactive } from "vue";
+
+export default {
+  setup() {
+    const datas = reactive<{ data: { [key: string]: string } }>({ data: {} });
+    const entry = function() {
+      datas.data[Date().toString()] = "出社";
+    };
+
+    const leave = function() {
+      datas.data[Date().toString()] = "退社";
+    };
+
+    return { 
+      datas, entry, leave
+    }
+  },
+};
+</script>
